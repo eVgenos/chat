0581a67b979822c8cb5efbfe10064c5181635715
eVgenos <evgenos2709@gmail.com>
2017-05-21 21:08:01 +0300

    Add posibility logout

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+
+
+  devise_for :users
+
+  root 'home#index'
+
+  resources :conversations, only: [:create] do
+    member do
+      post :close
+    end
+  end
+
+
+end
